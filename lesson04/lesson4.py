a32644d251021dd52c44c530f92c376f15e9f94b
Tony Weng <TonyW.Weng@gmail.com>
2025-02-14 14:39:33 -0500

    solved lesson 4

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,19 @@
 # Lesson 4
+import math
+
+fenceOne = len(input())
+fenceTwo = len(input())
+fenceThree = len(input())
+
+totalFence = fenceOne + fenceTwo + fenceThree
+paintCansOrders = totalFence / 12
+paintCansOrders = math.ceil(paintCansOrders)
+
+paintCansNeededBrought = paintCansOrders * 12
+remainingPaintCans = paintCansNeededBrought - totalFence
+totalCost = paintCansOrders * 14.95
+
+print(totalFence)
+print(remainingPaintCans)
+print(totalCost)
+print("Nice")
