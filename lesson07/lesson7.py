43cfb0d302f1756d5cb5fde8ca151ca5df7ea441
Tony Weng <TonyW.Weng@gmail.com>
2025-02-21 12:39:00 -0500

    lesson 7 solved

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,8 @@
 # Lesson 7
+from random import randrange
+difficultity = int(input("Enter how hard this check is: "))
+playerRoll = randrange(1, 21) #Range includes the first number but exculdes the last number
+if playerRoll >= difficultity:
+    print(f"The player passes the check because {playerRoll} >= {difficultity}")
+else:
+    print(f"The player has failed the check because {playerRoll} < {difficultity}, truly a sad moment")
