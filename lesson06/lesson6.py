7caa5e408dc44761d393724975327c37907ee030
Tony Weng <TonyW.Weng@gmail.com>
2025-02-21 12:32:51 -0500

    lesson 6 solved, also where is lesson 6 text?

--- lesson06/lesson6.py
+++ lesson06/lesson6.py
@@ -1,1 +1,24 @@
 # Lesson 6
+name = "Tony Weng"
+if name:
+    print(name)
+else:
+    print("There is literally nothing")
+
+nameGiven = input()
+if not name:
+    print("Name not given")
+else:
+    print(f"Hello {nameGiven}!")
+
+if "o" in nameGiven:
+    print("o is in the given name")
+else:
+    print("o is not in the given name")
+
+if "apple" not in ["banana", "kiwi", "fruit"]:
+    print("apple is not in the given list")
+else:
+    print("apple is in the given list")
+
+print("end")
