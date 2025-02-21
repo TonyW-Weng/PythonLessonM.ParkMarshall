d9232ae2234ce5ee5831ac18e7666a27d3732de0
Tony Weng <TonyW.Weng@gmail.com>
2025-02-20 21:56:46 -0500

    lesson 5 solved

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,22 @@
 # Lesson 5
+# testing
+start_money = float(input())
+cookies_sold = input()
+
+bigCookies = 0
+smallCookies = 0
+
+for currentCookie in cookies_sold:
+    if currentCookie == 'c':
+        smallCookies += 1
+    elif currentCookie == 'b':
+        bigCookies += 1
+    else:
+        print(f"{currentCookie} Not a valid sale item")
+
+totalNumCookie = bigCookies + smallCookies
+profit = (bigCookies*1.25) + (smallCookies*0.75)
+endDay = start_money + profit
+print(totalNumCookie)
+print(profit)
+print(endDay)
