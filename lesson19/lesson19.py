45f6e1d9c9a08825e4ea45105a9ba8627451a86e
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 18:31:50 -0500

    solved lesson 19

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,11 @@
 # Lesson 19
+number = int(input("Enter your number: "))
+totalFactors = 0 # counts 1 and itself
+for i in range(1, number+1):
+    if number % i == 0:
+        totalFactors += 1
+
+if totalFactors == 2:
+    print("IS a PRIME")
+else:
+    print("NOT a PRIME")
