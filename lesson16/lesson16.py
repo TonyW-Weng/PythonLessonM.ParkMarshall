65ddc64f4d19a261aabe25d1e1b333257f16bd2a
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 18:19:34 -0500

    solved lesson 16

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,10 @@
 # Lesson 16
+for num in range(1, 51):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(f"{num} does not divide by 3 or 5 perfectly")
