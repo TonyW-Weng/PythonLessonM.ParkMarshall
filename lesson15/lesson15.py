71607dd97f9377b3c79bb7e2bba17bccb9f7738d
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 18:14:34 -0500

    solved lesson 15 :)

--- lesson15/lesson15.py
+++ lesson15/lesson15.py
@@ -1,1 +1,23 @@
 # Lesson 15
+conditionVar = True
+password = "password"
+
+while conditionVar:
+    geuss = input("Geuss the password: ")
+    if geuss != password:
+        print("Try again")
+    else:
+        print("Good job, you can leave now")
+        conditionVar = False
+
+userInput = input("What fruit are you looking for?: ")
+fruits = ['Apple', 'Kiwi', 'Banana', 'Strawberry']
+found = False
+
+for fruitName in fruits:
+    if fruitName == userInput:
+        print("We found the fruit!")
+        found = True
+
+if not found:
+    print(f"{userInput} was not found")
