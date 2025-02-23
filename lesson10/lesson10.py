1b6701bea8598ce429fab7f3bcd4e4954c8ac51a
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 17:50:38 -0500

    lesson 10 solved

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,13 @@
 # Lesson 10
+digits = input("Enter the four digits: ")
+
+if digits[0] in {'8', '9'}:
+    if digits[1] == digits[2]:
+        if digits[3] in {'8', '9'}:
+            print("Is a telemarketer")
+        else:
+            print("Is not a telemarketer")
+    else:
+        print("Is not a telemarketer")
+else:
+    print("Is not a telemarketer")
