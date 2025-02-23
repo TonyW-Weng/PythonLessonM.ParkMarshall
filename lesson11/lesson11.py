7eef75e26c99ac2ef62db41b1c6f23ca303a3d43
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 17:57:04 -0500

    lesson 11 solved

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,16 @@
 # Lesson 11
+cordinates = input("Enter your two points: ")
+cordinates = cordinates.split(' ') # splits into two different ints "10 -11" -> [10, -11]
+cordinates = list(map(int, cordinates)) # map makes it iteratable and list makes a list
+
+x, y = cordinates
+if x > 0:
+    if y > 0:
+        print("We are in quadrant 1")
+    else:
+        print("We are in quadrant 4")
+else:
+    if y > 0:
+        print("We are in quadrant 2")
+    else:
+        print("We are in quadrant 3")
