121ff2724efaf210619b4c8234e26aac04effca5
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 20:50:44 -0500

    Solved lesson 24

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,15 @@
 # Lesson 24
+condition = True
+longestLength = 0
+longestName = ""
+
+while condition:
+    name = input("Enter a name or \" X \" to exit: ")
+    if name.lower().capitalize() == 'X':
+        condition = False
+        break
+    if len(longestName) < len(name):
+        longestName = name
+        longestLength = len(name)
+
+print(f"The longest name is {longestName} with a length of {longestLength}")
