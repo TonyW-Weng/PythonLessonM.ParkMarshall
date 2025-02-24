8f1011741d4abfc2a601c96261aa9855298bc06c
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 20:56:34 -0500

    solved lesson 25

--- lesson25/lesson25.py
+++ lesson25/lesson25.py
@@ -1,1 +1,21 @@
 # Lesson 25
+number = int(input("Enter your limit: "))
+copyNumber = number
+
+largest = 0
+
+while number % 2 == 0:
+    number //= 2
+
+    largest = max(largest, 2)
+
+if number != 1:
+    factor = 3
+    while number != 1:
+        if number % factor == 0:
+            largest = max(largest, factor)
+            number //= factor
+        else:
+            factor += 2
+
+print(f"{largest} is the prime factor for {copyNumber}")
