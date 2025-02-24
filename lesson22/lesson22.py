8b22795ea70067cc42364b8fa141b3efcca58b79
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 20:39:33 -0500

    solved lesson 22

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,13 @@
 # Lesson 22
+number = int(input("Enter your limit: "))
+fibZero = 0
+fibOne = 1
+fibN = 0
+
+for n in range(2, number+1):
+    fibN = fibOne + fibZero
+
+    fibZero = fibOne
+    fibOne = fibN
+
+print(f"Fibonacci({number}) is {fibN}")
