b9215d94c268842a73547853d3a5dc7a7e001401
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 20:36:02 -0500

    solved lesson 21

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,15 @@
 # Lesson 21
+numberInput = int(input("Enter your number: "))
+number = 0
+highestNumberOfFactors = 0
+
+for i in range (1, numberInput+1):
+    totalFactors = 0
+    for divide in range(1, i+1):
+        if i % divide == 0:
+            totalFactors += 1
+    if totalFactors > highestNumberOfFactors:
+        highestNumberOfFactors = totalFactors
+        number = i
+            
+print(f"The number with the highest amount of factors is: {number} with {highestNumberOfFactors} factors")
