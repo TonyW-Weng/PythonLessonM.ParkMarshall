1cba8df22a927f8fa6437541db1ed51081472b4f
Tony Weng <TonyW.Weng@gmail.com>
2025-02-23 20:46:16 -0500

    solved lesson 23

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,22 @@
 # Lesson 23
+
+condition = True
+Sum = 0
+numberOfInput = 0
+while condition:
+    user = input("Enter your number or \"Exit\" to exit: ")
+    if user.lower().capitalize() ==  'Exit':
+        condition = False
+        break
+    else:
+        mark = int(user)
+        if 0 <= mark <= 100:
+            Sum += mark
+            numberOfInput += 1
+        else:
+            print("Invalid mark")
+if numberOfInput == 0:
+    print("There is literally nothing")
+else:
+    average = Sum / numberOfInput
+    print(f"Your average is {average}")
