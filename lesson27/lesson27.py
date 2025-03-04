77254ff8b16b2537aea84d07dbd82aca00d5083d
Tony Weng <TonyW.Weng@gmail.com>
2025-03-04 14:26:11 -0500

    solved lesson 27

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,10 @@
 # Lesson 27
+def cleanString(text):
+    result = ''
+    for character in text:
+        if character.isalpha():
+            result += character.lower()
+    return result
+
+string = input("Enter your string: ")
+print(cleanString(string))
