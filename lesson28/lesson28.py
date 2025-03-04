4d54c135746e776dafaf2e33695d620f651230be
Tony Weng <TonyW.Weng@gmail.com>
2025-03-04 14:29:56 -0500

    solved lesson 28

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,12 @@
 # Lesson 28
+def checkWord(text):
+    condition = True
+    for i in range(len(text)):
+        if text[i] != text[len(text) - 1 - i]:
+            condition = False
+            return condition
+            break
+    return condition
+
+string = input("Enter your string: ")
+print(checkWord(string))
