0afa7b94da4b302ab7590bc065d5c65f48f70818
Tony Weng <TonyW.Weng@gmail.com>
2025-03-04 14:22:04 -0500

    solved lesson 26

--- lesson26/lesson26.py
+++ lesson26/lesson26.py
@@ -1,1 +1,13 @@
 # Lesson 26
+def factorCounter (number):
+    counter = 0
+    for i in range (1, number+1):
+        if number % i == 0:
+            counter += 1
+    return counter
+
+upperLimit = int(input())
+for i in range (1, upperLimit + 1):
+    factorSize = factorCounter(i)
+
+    print(f"{i} has {factorSize} number of factors")
