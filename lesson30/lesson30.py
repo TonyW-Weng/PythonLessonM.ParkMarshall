c12fea9499c17b9a7939e4050d31a541049e6057
Tony Weng <TonyW.Weng@gmail.com>
2025-03-05 14:04:21 -0500

    solved lesson 30

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,9 @@
 # Lesson 30
+patternLength = int(input("Enter your pattern length: "))
+pattern = '1'
+for i in range(patternLength):
+    print(pattern)
+    if i % 2 == 0:
+        pattern += '0'
+    else:
+        pattern += '1'
