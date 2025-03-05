4448578a25b685b76d2a8828d350e36ebe3b5569
Tony Weng <TonyW.Weng@gmail.com>
2025-03-05 14:35:15 -0500

    solved lesson 32

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,19 @@
 # Lesson 32
+
+def sortedString(textOne, textTwo):
+    print(textOne)
+    print(textTwo)
+    if  not textOne or not textTwo:
+        return []
+    else:
+        setWordTwo = set(textTwo)
+        setWordOne = set(textOne)
+        result = []
+        for character in setWordOne:
+            if character in setWordTwo:
+                result.append(character)
+        return sorted(result)
+
+wordOne = input("Enter your first string: ")
+wordTwo = input("Enter your second string: ")
+print(f"{sortedString(wordOne, wordTwo)}")
