815ba3b84f3b7a5cdad11d66112e48e1d8280b5c
Tony Weng <TonyW.Weng@gmail.com>
2025-03-05 14:00:58 -0500

    solved lesson 29

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,22 @@
 # Lesson 29
+def consonantsCounter(text, vowel = False):
+    count = 0
+
+    for character in text:
+        character = character.lower()
+        if character.isalpha() and character not in {'a', 'e', 'i', 'o', 'u'}:
+            count += 1
+    
+    if not vowel:
+        return count
+    else:
+        count = 0
+        for character in text:
+            character = character.lower()
+            if character.isalpha() and character in {'a', 'e', 'i', 'o', 'u'}:
+                count += 1
+        return count
+
+
+print(f"The amount of consonants in {consonantsCounter('hello, world!')}")
+print(f"The amount of consonants in {consonantsCounter('hello, world!', True)}")
