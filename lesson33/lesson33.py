8d945e8e65e08e23e22320d0419a697b24500c6d
Tony Weng <TonyW.Weng@gmail.com>
2025-03-05 14:45:00 -0500

    solved lesson 33

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,25 @@
 # Lesson 33
+def mean(list):
+    # Find the average
+    numberList = list
+    totalSum = sum(numberList)
+    size = len(numberList)
+    return totalSum / size
+
+def medium(list):
+    numberList = list
+    size = len(numberList)
+    medium = numberList[size//2]
+    return medium
+
+
+numberList = []
+loop = int(input("Enter your numbers for how many more numbers to enter: "))
+count = 0
+while count < loop:
+    number = int(input("Enter your number: "))
+    numberList.append(number)
+    count += 1
+
+print(f"{mean(numberList)}")
+print(f"{medium(numberList)}")
