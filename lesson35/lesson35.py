a0a8482472a0555aea175647aa9239f1ccc2c70e
Tony Weng <TonyW.Weng@gmail.com>
2025-03-17 14:35:39 -0400

    solved lesson 35

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,13 @@
 # Lesson 35
+def removeDupe(list):
+    testList = list
+    newList = []
+    for item in testList:
+        if item not in newList:
+            newList.append(item)
+
+    return newList
+
+testList = ["a", "b", "c", "d", "e", "a", "b", "c"]
+print(f"{testList}")
+print(f"{removeDupe(testList)}")
