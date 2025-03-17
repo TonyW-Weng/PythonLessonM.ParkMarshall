5ee9ad0e6737c18081a18c7715c13f5eabe62d5a
Tony Weng <TonyW.Weng@gmail.com>
2025-03-17 14:45:14 -0400

    solved lesson 36

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,17 @@
 # Lesson 36
+# finding the factors of a number and prime of a number
+
+def factor(number):
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
+
+def primes(number):
+
+    return len(factor(number)) == 2
+print(f"Factors of 13: {factor(13)}")
+print(f"Factors of 36: {factor(36)}")
+print(f"Is 13 Prime: {primes(13)}")
+print(f"Is 36 Prime: {primes(36)}")
