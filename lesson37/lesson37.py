ff533e1a0cacdb911065e5dafc245a39e0c7f81e
Tony Weng <TonyW.Weng@gmail.com>
2025-03-20 14:29:24 -0400

    solved lesson 37

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,20 @@
 # Lesson 37
+def convert(text):
+    newText = ''
+    count = 1
+    for i in range(1, len(text)):
+        print(text[i])
+        if text[i] == text[i - 1]:
+            count += 1
+        else:
+            newText += text[i - 1]
+            newText += str(count)
+            count = 1
+    newText += text[-1] + str(count)
+    if len(text) >= len(newText):
+        return newText
+    else:
+        return text
+
+userInput = input("Enter your text: ")
+print(f"The new text is: {convert(userInput)}")
