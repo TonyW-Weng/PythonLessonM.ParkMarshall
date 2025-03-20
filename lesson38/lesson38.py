28b8116aaedac12d5be7ece4cbf75e54aa4998b4
Tony Weng <TonyW.Weng@gmail.com>
2025-03-20 14:37:28 -0400

    solved lesson 38

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,14 @@
 # Lesson 38
+def isPal(text):
+    return text == text[::-1]
+
+palNumber = []
+
+for num1 in range(999, 99, -1):
+    for num2 in range(999, 99, -1):
+        currentProduct = num1*num2
+
+        if isPal(str(currentProduct)):
+            palNumber.append(currentProduct)
+
+print(f"The largest pal of the product of given numbers is {max(palNumber)}")
