7ddc842f4140dc4612b3d1dba9567e95d8fe3925
Tony Weng <TonyW.Weng@gmail.com>
2025-03-20 14:49:04 -0400

    solved lesson 39

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,13 @@
 # Lesson 39
+
+def commonDiv(num1, num2):
+    for divide in range(min(num1, num2), 0, -1):
+        if num1 % divide == 0 and num2 % divide == 0:
+            return divide
+        
+    return 1
+
+
+numberOne = int(input("Enter your first number: "))
+numberTwo = int(input("Enter your second number: "))
+print(f"The greatest common divider of {numberOne} and {numberTwo} are {commonDiv(numberOne, numberTwo)}")
