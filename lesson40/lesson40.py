9ecce70bab24c2c9e14a3c8a9c07008c536cd0d3
Tony Weng <TonyW.Weng@gmail.com>
2025-03-21 14:18:24 -0400

    solved lesson 40

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,14 @@
 # Lesson 40
+def primeNumber(number):
+    primes = []
+    for i in range(1, number+1):
+        Count = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                Count += 1
+        if Count == 2:
+            primes.append(i)
+    return primes
+
+number = int(input("Enter your number: "))
+print(f"{primeNumber(number)}")
