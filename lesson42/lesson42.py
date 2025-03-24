ab4ce81fb214ad318f8b0f392abaa9731e8515da
Tony Weng <TonyW.Weng@gmail.com>
2025-03-24 13:54:30 -0400

    solved lesson 42

--- lesson42/lesson42.py
+++ lesson42/lesson42.py
@@ -1,1 +1,12 @@
 # Lesson 42
+def possibleSum(aList, target):
+    for i, value in enumerate(aList):
+        newTarget = target - value
+        if newTarget in aList[i+1:]:
+            return True
+    return False
+
+test = [1, 3, 5, 8, 12, 13, 22]
+target = 23
+
+print(possibleSum(test, target))
