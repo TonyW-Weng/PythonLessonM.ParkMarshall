0d2a0e4d3217e78292f5072929f45e90bd18a430
Tony Weng <TonyW.Weng@gmail.com>
2025-03-24 14:30:43 -0400

    solved lesson 47

--- lesson47/lesson47.py
+++ lesson47/lesson47.py
@@ -1,1 +1,11 @@
 # Lesson 47
+def repeatedSum(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return num + repeatedSum(num - 1)
+
+number = int(input("Enter your number: "))
+print(repeatedSum(number))
