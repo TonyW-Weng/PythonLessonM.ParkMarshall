ac0e8582c43070bb0e0fd32fc2ab396c39b58898
Tony Weng <TonyW.Weng@gmail.com>
2025-03-24 14:11:45 -0400

    solved lesson 44

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,13 @@
 # Lesson 44
+def frequency(word):
+    cleanWord = sorted(word.lower())
+    answer = {}
+    for character in cleanWord:
+        if character not in answer:
+            answer[character] = 1
+        else:
+            answer[character] += 1
+    return answer
+
+userWord = input("Enter your word: ")
+print(frequency(userWord))
