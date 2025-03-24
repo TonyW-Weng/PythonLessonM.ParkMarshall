337a6aa1d15b95ef95df7a8bdc3683b4685125d3
Tony Weng <TonyW.Weng@gmail.com>
2025-03-24 14:18:36 -0400

    solved lesson 45

--- lesson45/lesson45.py
+++ lesson45/lesson45.py
@@ -1,1 +1,24 @@
 # Lesson 45
+#Q1
+def dic(aList):
+    answer = {}
+    for item in aList:
+        if item not in answer:
+            answer[item] = len(item)
+    return answer
+
+test = ["apple", "banana", "pineapple", "watermelon", "strawberry"]
+print(dic(test))
+
+#Q2
+def fib(num):
+    result = {0:0, 1:1}
+    if num in result:
+        return result
+    else:
+        for i in range(2, num+1):
+            result[i] = result[i - 1] + result[i - 2]
+
+        return result
+number = int(input("Enter your number: "))
+print(fib(number))
